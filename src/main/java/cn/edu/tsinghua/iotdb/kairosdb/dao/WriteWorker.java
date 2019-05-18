1950064f815870f21725c1c8dd5157850b2ddcc3
liuruiyiyang <245422695@qq.com>
2019-05-18 11:46:48 +0800

    debug workder

--- src/main/java/cn/edu/tsinghua/iotdb/kairosdb/dao/WriteWorker.java
+++ src/main/java/cn/edu/tsinghua/iotdb/kairosdb/dao/WriteWorker.java
@@ -3,9 +3,6 @@ package cn.edu.tsinghua.iotdb.kairosdb.dao;
 import cn.edu.tsinghua.iotdb.kairosdb.http.rest.json.DataPointsParser;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
 import java.io.StringReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,18 +21,19 @@ public class WriteWorker extends Thread {
   public void run() {
 
     while (true) {
-      String json = MessageQueue.getInstance().poll();
-      LOGGER.info("json is: {}", json);
-      if (json.length() > 1) {
-        try {
+      try {
+        String json = MessageQueue.getInstance().poll();
+        LOGGER.info("json is: {}", json);
+        if (json != null && json.length() > 1) {
 
           StringReader stringReader = new StringReader(json);
           DataPointsParser parser = new DataPointsParser(stringReader, gson);
 
           parser.parse();
-        } catch (Exception e) {
-          LOGGER.error("Write worker execute parser.parse() failed because ", e);
+
         }
+      } catch (Exception e) {
+        LOGGER.error("Write worker execute parser.parse() failed because ", e);
       }
     }
 
