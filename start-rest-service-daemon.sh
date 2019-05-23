bd4707f76bce584663b7b50beca428ba85162de7
liuruiyiyang <245422695@qq.com>
2019-05-23 13:31:52 +0800

    use absolute path

--- start-rest-service-daemon.sh
+++ start-rest-service-daemon.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-./start-rest-service.sh > /dev/null 2>&1 &
+if [ -z "${REST_HOME}" ]; then
+  export REST_HOME="$(cd "`dirname "$0"`"; pwd)"
+fi
+
+$REST_HOME/start-rest-service.sh > /dev/null 2>&1 &
