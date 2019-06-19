22dbd298430f227748634128e8c4353c97089a82
liuruiyiyang <245422695@qq.com>
2019-06-19 14:21:30 +0800

    add tools package

---
+++ tools/export/src/main/java/kairosdb/export/csv/ExportToCsv.java
@@ -0,0 +1,11 @@
+package kairosdb.export.csv;
+
+/**
+ * Hello world!
+ */
+public class ExportToCsv {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ tools/export/src/test/java/kairosdb/export/csv/ExportToCsvTest.java
@@ -0,0 +1,19 @@
+package kairosdb.export.csv;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ExportToCsv.
+ */
+public class ExportToCsvTest {
+
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
