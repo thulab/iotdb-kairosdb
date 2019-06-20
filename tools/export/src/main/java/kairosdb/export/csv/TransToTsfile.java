565f298f9b2dbd6fa84fd6a71132e6276f965a98
EJTTianyu <1653503269@qq.com>
2019-06-20 10:14:10 +0800

    transfer to tsfile complete

---
+++ tools/export/src/main/java/kairosdb/export/csv/TransToTsfile.java
@@ -0,0 +1,117 @@
+package kairosdb.export.csv;
+
+import java.util.List;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSEncoding;
+import org.apache.iotdb.tsfile.utils.Binary;
+import org.apache.iotdb.tsfile.write.TsFileWriter;
+import org.apache.iotdb.tsfile.write.record.TSRecord;
+import org.apache.iotdb.tsfile.write.record.datapoint.DataPoint;
+import org.apache.iotdb.tsfile.write.record.datapoint.FloatDataPoint;
+import org.apache.iotdb.tsfile.write.record.datapoint.IntDataPoint;
+import org.apache.iotdb.tsfile.write.record.datapoint.StringDataPoint;
+import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
+
+public class TransToTsfile {
+
+  public static void transToTsfile(String csvPath, String tsPath) {
+    try {
+      String path = tsPath;
+      File f = new File(path);
+      if (f.exists()) {
+        f.delete();
+      }
+      TsFileWriter tsFileWriter = new TsFileWriter(f);
+      BufferedReader csvReader = new BufferedReader(new FileReader(csvPath));
+      String header = csvReader.readLine();
+//      Long time = Long.parseLong(header.split(",")[0]);
+      String[] sensorFull = Arrays.copyOfRange(header.split(","), 1, header.split(",").length);
+      ArrayList<String> sensorList = new ArrayList<>();
+      for (String sensor_ : sensorFull) {
+        sensorList.add(sensor_);
+      }
+      String device = sensorList.get(1).substring(0, sensorList.get(1).lastIndexOf("."));
+      for (int i = 0; i < sensorList.size(); i++) {
+        sensorList.set(i, sensorList.get(i).replace(device, "").substring(1));
+      }
+      List<TSDataType> tsDataTypes = Arrays.asList(new TSDataType[sensorList.size()]);
+      System.out.println(sensorList);
+      String intRegex = "[0-9]+";
+      String floatRegex = "[0-9]+.[0-9]+";
+      String line = null;
+      while ((line = csvReader.readLine()) != null) {
+        // construct TSRecord
+//        System.out.println(line);
+        Long time = Long.parseLong(line.split(",")[0]);
+        TSRecord tsRecord = new TSRecord(time, device);
+        String[] points = Arrays.copyOfRange(line.split(","), 1, line.split(",").length);
+        for (int i = 0; i < points.length; i++) {
+          if (points[i].matches(intRegex)) {
+            if (tsDataTypes.get(i) == null) {
+              tsDataTypes.set(i, TSDataType.INT32);
+              tsFileWriter.addMeasurement(new MeasurementSchema(sensorList.get(i),
+                  TSDataType.INT32, TSEncoding.TS_2DIFF));
+              DataPoint intPoint=new IntDataPoint(sensorList.get(i),Integer.parseInt(points[i]));
+              tsRecord.addTuple(intPoint);
+              System.out.println(String.format("插入整数数据点:%s",Integer.parseInt(points[i])));
+            }
+            else {
+              DataPoint intPoint=new IntDataPoint(sensorList.get(i),Integer.parseInt(points[i]));
+              tsRecord.addTuple(intPoint);
+              System.out.println(String.format("插入整数数据点:%s",Integer.parseInt(points[i])));
+            }
+          }
+          else if (points[i].matches(floatRegex)){
+            if (tsDataTypes.get(i) == null) {
+              tsDataTypes.set(i, TSDataType.FLOAT);
+              tsFileWriter.addMeasurement(new MeasurementSchema(sensorList.get(i),
+                  TSDataType.FLOAT, TSEncoding.RLE));
+              DataPoint floatPoint=new FloatDataPoint(sensorList.get(i),Float.parseFloat(points[i]));
+              tsRecord.addTuple(floatPoint);
+              System.out.println(String.format("插入浮点数据点:%s",Float.parseFloat(points[i])));
+            }
+            else {
+              DataPoint floatPoint=new FloatDataPoint(sensorList.get(i),Float.parseFloat(points[i]));
+              tsRecord.addTuple(floatPoint);
+              System.out.println(String.format("插入整数数据点:%s",Float.parseFloat(points[i])));
+            }
+          }
+          else {
+            if (tsDataTypes.get(i) == null) {
+              tsDataTypes.set(i, TSDataType.TEXT);
+              tsFileWriter.addMeasurement(new MeasurementSchema(sensorList.get(i),
+                  TSDataType.TEXT, TSEncoding.PLAIN));
+              DataPoint textPoint=new StringDataPoint(sensorList.get(i), Binary.valueOf(points[i]));
+              tsRecord.addTuple(textPoint);
+              System.out.println(String.format("插入字符数据点:%s",Binary.valueOf(points[i])));
+            }
+            else {
+              DataPoint textPoint=new StringDataPoint(sensorList.get(i), Binary.valueOf(points[i]));
+              tsRecord.addTuple(textPoint);
+              System.out.println(String.format("插入字符数据点:%s",Binary.valueOf(points[i])));
+            }
+          }
+        }
+        tsFileWriter.write(tsRecord);
+      }
+      tsFileWriter.close();
+    } catch (Throwable e) {
+      e.printStackTrace();
+      System.out.println(e.getMessage());
+    }
+
+  }
+
+  public static void main(String[] args) {
+    transToTsfile("/Users/tianyu/git_project/iotdb-kairosdb/tools/export/res/test.csv",
+        "/Users/tianyu/git_project/iotdb-kairosdb/tools/export/res/test.tsfile");
+  }
+
+}
