2903d34cfbb4de4f2ccaf199d096662b9b65f683
liuruiyiyang <245422695@qq.com>
2019-06-25 15:52:55 +0800

    change threshold

--- src/main/java/cn/edu/tsinghua/iotdb/kairosdb/http/rest/json/DataPointsParser.java
+++ src/main/java/cn/edu/tsinghua/iotdb/kairosdb/http/rest/json/DataPointsParser.java
@@ -35,7 +35,7 @@ public class DataPointsParser {
   private static AtomicLong parseJsonAccumulatedTime = new AtomicLong(0);
   private static AtomicLong jdbcAccumulatedTime = new AtomicLong(0);
   private static AtomicLong parseTotalAccumulatedTime = new AtomicLong(0);
-  private static final int OUTPUT_THRESHOLD = 1000;
+  private static final int OUTPUT_THRESHOLD = 5000;
   private final Reader inputStream;
   private final Gson gson;
 
