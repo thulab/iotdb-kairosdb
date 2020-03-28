311ba7c28754702973ff686fdbe91a7d035606ae
liuruiyiyang <245422695@qq.com>
2020-03-28 17:18:47 +0800

    add multiple db node test script

--- test_dir/mul-db-test.sh
+++ test_dir/mul-db-test.sh
@@ -4,19 +4,24 @@ USER=fit
 WORK_DIR=/home/fit/liurui/distributed_ikr
 IOTDB_PATH=$WORK_DIR/iotdb-server-0.9.0
 IP_ARRAY=(4 5 11 14 17 19 20 21)
+NET_ZONE=192.168.130
+
+### 在每个节点上安装IoTDB
+#for i in ${IP_ARRAY[@]}
+#do
+#  echo "initializing $USER@$NET_ZONE.$i"
+#  ssh $USER@$NET_ZONE.$i "rm -rf $WORK_DIR;mkdir -p $WORK_DIR"
+#  scp -r iotdb-server-0.9.0/ $USER@$NET_ZONE.$i:$WORK_DIR
+#done
 
 for i in ${IP_ARRAY[@]}
 do
-  scp -r iotdb-server-0.9.0/ $USER@192.168.130.$i:$WORK_DIR
-done
-
-for i in ${IP_ARRAY[@]}
-do
-  ssh $USER@192.168.130.$i "sh $IOTDB_PATH/sbin/stop-server.sh"
-  ssh $USER@192.168.130.$i "rm -rf $IOTDB_PATH/data"
-  ssh $USER@192.168.130.$i "rm -rf $IOTDB_PATH/logs"
+  ssh $USER@$NET_ZONE.$i "sh $IOTDB_PATH/sbin/stop-server.sh"
+  ssh $USER@$NET_ZONE.$i "rm -rf $IOTDB_PATH/data"
+  ssh $USER@$NET_ZONE.$i "rm -rf $IOTDB_PATH/logs"
   sleep 2
-  ssh $USER@192.168.130.$i "sh $IOTDB_PATH/sbin/start-server.sh > /dev/null 2>&1 &"
+  ssh $USER@$NET_ZONE.$i "sh $IOTDB_PATH/sbin/start-server.sh > /dev/null 2>&1 &"
+  echo "start databases on $USER@$NET_ZONE.$i"
 done
 
-echo "started databases on all nodes"
+echo "All databases started"
